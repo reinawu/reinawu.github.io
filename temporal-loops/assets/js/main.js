02e6ffd6628e36d5e170756b4332e4c5d1f920f7
reinawu <reina971004@gmail.com>
2020-06-24 17:06:36 +1000

    tl updt

--- temporal-loops/assets/js/main.js
+++ temporal-loops/assets/js/main.js
@@ -10,48 +10,25 @@ $(document).ready(function () {
   });
 });
 
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
 
 
-var slideIndex = 1;
-showSlides(slideIndex);
+$( document ).ready(function() {
+  console.log("I'm Ready");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+$( ".header" ).click(function() {
+  $( ".toggle" ).slideToggle( "slow" );
+});
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+});
 
 
 
+$( document ).ready(function() {
+    
+ $( function() {
+    $( ".drag-image").draggable();
+  } );
+
+});
 
 
